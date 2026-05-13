33c7f43d28b571430b8ed4ea9b397af9d6e6125b
Gabriel Señar <195650917+IT-Abby@users.noreply.github.com>
2026-05-13 23:39:48 +0800

    Implemented Sign In UI

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,15 +1,59 @@
-import { Link } from "expo-router";
+import { icons } from "@/constants/icons";
+import { Link, router } from "expo-router";
 import { styled } from "nativewind";
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 const SignIn = () => {
   return (
-    <SafeAreaView>
-      <Text>Sign In</Text>
-      <Link href="/(tabs)">Go to Tabs</Link>
-      <Link href="/(auth)/sign-up">Create Account</Link>
+    <SafeAreaView className="flex-1 bg-background">
+      <View className="auth-banner">
+        <View className="auth-logo">
+          <Image source={icons.banner} alt="banner"></Image>
+          <View className="auth-title">
+            <Text className="auth-app-name">Recurlly</Text>
+            <Text className="auth-sub-name">SMART BILLING</Text>
+          </View>
+        </View>
+
+        <View className="welcome-cont">
+          <Text className="welcome-text">Welcome back!</Text>
+          <Text className="welcome-sub-text">
+            Sign-In to continue managing your Subscriptions
+          </Text>
+        </View>
+
+        <View className="auth-form">
+          <Text className="auth-input-label">Email</Text>
+          <TextInput
+            className="auth-input"
+            placeholder="Enter your email"
+          ></TextInput>
+
+          <Text className="auth-input-label">Password</Text>
+          <TextInput
+            className="auth-input"
+            placeholder="Enter your password"
+          ></TextInput>
+
+          <View className="auth-button-cont">
+            <TouchableOpacity
+              className="sign-in-button"
+              onPress={() => router.replace("/(tabs)")}
+            >
+              <Text className="sign-in-text">Sign-In</Text>
+            </TouchableOpacity>
+
+            <View className="footer">
+              <Text className="sign-up-message">New to recurly?</Text>
+              <Link className="sign-up-text" href="/(auth)/sign-up">
+                Create Account
+              </Link>
+            </View>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
